b6db26317a2d3b1ca196d3dc8265bf5c700a0807
basesnel <basesnel@gmail.com>
2023-02-01 23:00:30 +0200

    Homework edit.

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ const loadMoreBtn = new LoadMoreBtn({
   hidden: true,
 });
 
+const options = { captionsData: 'alt', captionDelay: 250 };
+
+var lightbox = new SimpleLightbox('.gallery a', options);
+
 const imagesApiService = new ImagesApiService();
 let countHits = 0;
 
@@ -27,7 +31,7 @@ function onSearch(e) {
   imagesApiService.query = this.searchQuery.value.trim();
   this.searchQuery.value = imagesApiService.query;
 
-  if (imagesApiService.query === '') {
+  if (!imagesApiService.query) {
     clearImagesContainer();
     return Notify.warning('Please enter no empty value.');
   }
@@ -35,6 +39,7 @@ function onSearch(e) {
   countHits = 0;
   loadMoreBtn.hide();
   imagesApiService.resetPage();
+  imagesApiService.resetCountHits();
   clearImagesContainer();
 
   toFetchImages();
@@ -45,43 +50,46 @@ function onLoadMore() {
 }
 
 async function toFetchImages() {
-  try {
-    loadMoreBtn.disable();
-    const imagesData = await imagesApiService.fetchImages();
+  // try {
+  loadMoreBtn.disable();
+  const imagesData = await imagesApiService.fetchImages();
 
-    if (!imagesData.totalHits) throw new Error();
-
-    if (!(imagesApiService.page > 2) && imagesData.totalHits) {
-      Notify.success(`Hooray! We found ${imagesData.totalHits} images.`);
-    }
-
-    loadMoreBtn.show();
-
-    const { height: galleryBottom } = document
-      .querySelector('.gallery')
-      .getBoundingClientRect();
-
-    appendImagesMarkup(imagesData.hits);
-
-    window.scrollBy({
-      top: galleryBottom - window.pageYOffset,
-      behavior: 'smooth',
-    });
-
-    countHits += imagesData.hits.length;
-    if (countHits === imagesData.totalHits) {
-      loadMoreBtn.hide();
-      return Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
-    }
-
-    loadMoreBtn.enable();
-  } catch (error) {
-    Notify.failure(
+  if (!imagesData.totalHits) {
+    return Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
+
+  if (imagesApiService.success) {
+    Notify.success(`Hooray! We found ${imagesData.totalHits} images.`);
+  }
+
+  loadMoreBtn.show();
+
+  const { height: galleryBottom } = document
+    .querySelector('.gallery')
+    .getBoundingClientRect();
+
+  appendImagesMarkup(imagesData.hits);
+
+  lightbox.refresh();
+
+  window.scrollBy({
+    top: galleryBottom - window.pageYOffset,
+    behavior: 'smooth',
+  });
+
+  if (imagesApiService.endOfSearch) {
+    loadMoreBtn.hide();
+    return Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+
+  loadMoreBtn.enable();
+  // } catch (error) {
+  // console.log(error);
+  // }
 }
 
 function appendImagesMarkup(images) {
@@ -98,7 +106,7 @@ function appendImagesMarkup(images) {
       }) =>
         `<a class="photo-link" href="${largeImageURL}">
     <div class="photo-card">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <img class="photo-img image" src="${webformatURL}" alt="${tags}" loading="lazy" />
       <div class="info">
         <p class="info-item">
           <b>Likes</b> ${likes}
@@ -120,11 +128,6 @@ function appendImagesMarkup(images) {
 
   // console.log(markup);
   refs.imagesContainer.insertAdjacentHTML('beforeend', markup);
-
-  const options = { captionsData: 'alt', captionDelay: 250 };
-
-  var lightbox = new SimpleLightbox('.gallery a', options);
-  lightbox.refresh();
 }
 
 function clearImagesContainer() {
--- src/js/images-service.js
+++ src/js/images-service.js
@@ -4,33 +4,48 @@ export default class ImagesApiService {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
+    this.success = false;
+    this.countHits = 0;
+    this.endOfSearch = false;
   }
 
   async fetchImages() {
     let url = 'https://pixabay.com/api/';
     let page = this.page;
 
-    const res = await axios
-      .get(url, {
-        params: {
-          key: '33045581-3a3af9a2074d1c4024adb3324',
-          q: this.searchQuery,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: 'true',
-          per_page: '40',
-          page: page,
-        },
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        maxRedirects: 0,
-      })
-      .then(response => {
-        this.incrementPage();
-        return response.data;
-      });
-    return res;
+    const response = await axios.get(url, {
+      params: {
+        key: '33045581-3a3af9a2074d1c4024adb3324',
+        q: this.searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: '40',
+        page: page,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      maxRedirects: 0,
+    });
+    const data = response.data;
+    this.incrementPage();
+
+    // if (!data.totalHits) throw new Error();
+
+    if (!(this.page > 2) && data.totalHits) {
+      this.success = true;
+    } else {
+      this.success = false;
+    }
+
+    this.countHits += data.hits.length;
+
+    this.endOfSearch = false;
+    if (this.countHits === data.totalHits) {
+      this.endOfSearch = true;
+    }
+    return data;
   }
 
   incrementPage() {
@@ -39,6 +54,10 @@ export default class ImagesApiService {
 
   resetPage() {
     this.page = 1;
+  }
+
+  resetCountHits() {
+    this.countHits = 0;
   }
 
   get query() {
